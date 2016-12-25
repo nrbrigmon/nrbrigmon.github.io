0821a13e94830a5ce07348e036700d914a558067
Nate Brigmon <nathanbrigmon@Nathans-MacBook-Pro.local>
2016-12-25 12:19:34 -0600

    animated navbar and project buttons

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
 
+  $("nav.navbar ul li a").click(function(){
+        
+        $("nav.navbar ul li a").removeClass('active');
+        $(this).addClass('active');
+   });
 
-	$("#portfolio-contant-active").mixItUp();
-
+  $(window).scroll(function() {
+	  if ($(document).scrollTop() > 100) {
+	    $('nav').addClass('shrink');
+	  } else {
+	    $('nav').removeClass('shrink');
+	  }
+	});
 
 	$("#testimonial-slider").owlCarousel({
 	    paginationSpeed : 500,      
@@ -43,6 +53,27 @@ $(document).ready(function(){
 	$('.counter').counterUp({
         delay: 10,
         time: 1000
+    });
+
+  $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        $(".filter-button").removeClass('current');
+        $(this).addClass('current');
+
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
     });
 
 
